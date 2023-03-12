ddc9a693a3b5abbecf4dd0907bd3232e6b11b2e3
LuthyX <peteradegoke81@gmail.com>
2023-03-12 17:59:02 +0100

    feat: created and set up Product Model

---
+++ springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/model/Product.java
@@ -0,0 +1,27 @@
+package com.luti.sales_inventory.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer inStock;
+
+    @OneToMany
+    @JsonIgnore
+    private List<WebOrder> orders = new ArrayList<>();
+
+}
