899e6d7dbcfa9c0cebd8a8dfd8712d4f3c823da1
LuthyX <peteradegoke81@gmail.com>
2023-03-12 17:57:42 +0100

    feat: created and added Product Repository

---
+++ springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.luti.sales_inventory.repository;
+
+import com.luti.sales_inventory.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
