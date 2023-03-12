da47843446906a9eb730dc8f6d061ad48bbed9b9
LuthyX <peteradegoke81@gmail.com>
2023-03-12 17:24:49 +0100

    inital setup

---
+++ springboot-interview-project/src/main/java/com/luti/springbootinterviewproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.luti.springbootinterviewproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootInterviewProjectApplication.class);
+	}
+
+}
---
+++ springboot-interview-project/src/test/java/com/luti/springbootinterviewproject/SpringbootInterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luti.springbootinterviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
