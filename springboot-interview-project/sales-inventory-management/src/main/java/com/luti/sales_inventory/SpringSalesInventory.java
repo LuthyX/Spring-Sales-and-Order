a768fc7a64d6d3a17f92c132ee188b64b6c26cf4
LuthyX <peteradegoke81@gmail.com>
2023-03-13 06:18:40 +0100

    feat: added CommandLine runner to input data at start up

--- springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/SpringSalesInventory.java
+++ springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/SpringSalesInventory.java
@@ -1,2 +1,27 @@
-package com.luti.sales_inventory;public class SpringSalesInventory {
+package com.luti.sales_inventory;
+
+import com.luti.sales_inventory.request.CreateCustomerRequest;
+import com.luti.sales_inventory.request.CreateProductRequest;
+import com.luti.sales_inventory.service.CustomerService;
+import com.luti.sales_inventory.service.ProductService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringSalesInventory {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSalesInventory.class);
+    }
+    @Bean
+    CommandLineRunner run(CustomerService customerService, ProductService productService) {
+        return args -> {
+            customerService.createCustomer(new CreateCustomerRequest("John", "08104057"));
+            customerService.createCustomer(new CreateCustomerRequest("Job", "081040689"));
+            productService.addProduct(new CreateProductRequest("Game1", "Very nice game",10, 4.5));
+            productService.addProduct(new CreateProductRequest("Game2", "Very nice game",5, 4.5));
+        };
+
+    }
 }
