b31014062864d7111fd0215af36aa69d94a524f3
LuthyX <peteradegoke81@gmail.com>
2023-03-13 06:15:12 +0100

    feat: Unit test

---
+++ springboot-interview-project/sales-inventory-management/src/test/java/com/luti/sales_inventory/Sales_InventoryTest.java
@@ -0,0 +1,12 @@
+package com.luti.sales_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Sales_InventoryTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
