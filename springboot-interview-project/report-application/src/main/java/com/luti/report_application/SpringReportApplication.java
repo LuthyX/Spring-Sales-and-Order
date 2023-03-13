4851aff061dc0067dc28ab81b607e01fd881f42f
LuthyX <peteradegoke81@gmail.com>
2023-03-13 06:28:06 +0100

    feat: Initial Springboot setup

---
+++ springboot-interview-project/report-application/src/main/java/com/luti/report_application/SpringReportApplication.java
@@ -0,0 +1,11 @@
+package com.luti.report_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReportApplication.class);
+    }
+}
