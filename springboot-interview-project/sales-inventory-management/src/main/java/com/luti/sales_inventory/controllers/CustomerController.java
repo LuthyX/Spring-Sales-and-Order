aeb4d23a73187b5fb0705fc04968c18dd5f2dff5
LuthyX <peteradegoke81@gmail.com>
2023-03-13 02:14:50 +0100

    feat: created and set up methods for Customer Controller

---
+++ springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/controllers/CustomerController.java
@@ -0,0 +1,43 @@
+package com.luti.sales_inventory.controllers;
+
+import com.luti.sales_inventory.model.Customer;
+import com.luti.sales_inventory.request.CreateCustomerRequest;
+import com.luti.sales_inventory.response.BaseResponse;
+import com.luti.sales_inventory.response.ListResponse;
+import com.luti.sales_inventory.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    private final static String CUSTOMER_CREATED = "Customer successfully created";
+
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping()
+    private ResponseEntity<ListResponse> getCustomers(){
+        ListResponse response = new ListResponse();
+        List<Customer> customers = customerService.getCustomers();
+        response.setSuccess(true);
+        response.setData(customers);
+        return ResponseEntity.status(200).body(response);
+    }
+    private ResponseEntity<BaseResponse> createCustomer(@RequestBody CreateCustomerRequest request){
+        BaseResponse response = new BaseResponse();
+        Customer customer = customerService.createCustomer(request);
+        response.setSuccess(true);
+        response.setData(customer);
+        response.setMsg(CUSTOMER_CREATED);
+        return ResponseEntity.status(201).body(response);
+    }
+}
