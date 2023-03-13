077a89ddc05d8effa209e9eb71dc64504da0dbb5
LuthyX <peteradegoke81@gmail.com>
2023-03-13 06:12:15 +0100

    feat: added ToString method

--- springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/request/OrderRequest.java
+++ springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/request/OrderRequest.java
@@ -1,8 +1,12 @@
 package com.luti.sales_inventory.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequest {
     private Long customer_id;
     private Long product_id;
