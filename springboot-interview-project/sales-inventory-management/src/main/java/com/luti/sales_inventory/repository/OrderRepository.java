ff96938fd45f7fa58bbea9f47367497d912dfa3b
LuthyX <peteradegoke81@gmail.com>
2023-03-13 02:23:58 +0100

    feat: wrote query and method to return available products

--- springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.luti.sales_inventory.repository;
-
-import com.luti.sales_inventory.model.WebOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<WebOrder, Long> {
-}
--- springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/repository/ProductRepository.java
+++ springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/repository/ProductRepository.java
@@ -2,8 +2,13 @@ package com.luti.sales_inventory.repository;
 
 import com.luti.sales_inventory.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p from Product p WHERE p.inStock >= 1")
+    List<Product> findAvailableProducts();
 }
