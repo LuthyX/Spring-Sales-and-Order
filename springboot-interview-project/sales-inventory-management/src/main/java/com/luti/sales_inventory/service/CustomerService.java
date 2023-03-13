dc03c7ad9754bd6ea71e3ec7b5f5897943127aa2
LuthyX <peteradegoke81@gmail.com>
2023-03-13 02:31:14 +0100

    feat: updated

--- springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/service/CustomerService.java
+++ springboot-interview-project/sales-inventory-management/src/main/java/com/luti/sales_inventory/service/CustomerService.java
@@ -1,7 +1,28 @@
 package com.luti.sales_inventory.service;
 
+import com.luti.sales_inventory.model.Customer;
+import com.luti.sales_inventory.repository.CustomerRepository;
+import com.luti.sales_inventory.request.CreateCustomerRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+    private CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer createCustomer(CreateCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.getName());
+        customer.setPhonenumber(request.getPhonenumber());
+        customerRepository.save(customer);
+        return customer;
+    }
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
+    }
 }
